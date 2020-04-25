d098c6bbc8a59b50c2d6963d6f9924bb535d88ac
LamprosTolis <37981510+LamprosTolis@users.noreply.github.com>
2020-04-25 11:45:37 +0300

    Add project files.

---
+++ CrmApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CrmApp
+{
+  class Program
+  {
+    class MyClass
+      {
+
+
+      public static void Main()
+      {
+        string returnedProduct;
+        string message = "Give me the product name:";
+
+
+        //method call
+        returnedProduct = DoWork(message);
+        Console.WriteLine(returnedProduct);
+        Console.ReadLine();
+      }
+
+
+
+
+      //Declaration of a method
+      static string DoWork(string message)
+      {
+
+          string Product;
+
+          Console.WriteLine("Hello Code.Hub buds!");
+          Product = Console.ReadLine();
+
+          //Console.WriteLine(Product);
+
+          return Product;
+      }
+
+
+
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
