43212cc22acbf960e49f4f3725a8117edce58456
LamprosTolis <37981510+LamprosTolis@users.noreply.github.com>
2020-04-25 13:43:39 +0300

    OP implementations of the examples

---
+++ CrmApp/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrmApp
+{
+  class Product
+  {
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+
+
+    public void Print()
+    {
+      Console.WriteLine(Code);
+      Console.WriteLine(Name);
+      Console.WriteLine(Price);
+      Console.WriteLine(Quantity);
+      Console.WriteLine(GetTotalCost());
+      Console.WriteLine();
+    }
+
+    public void IncreasePrice (decimal percentage)
+    {
+      Price *= (1+ percentage);
+    }
+
+    public decimal GetTotalCost()
+    {
+      decimal totalCost;
+      totalCost = Price * Quantity;
+      return totalCost;
+    }
+
+    public void setCode(string code)
+    {
+      Code = code;
+    }
+
+    public string GetCode()
+    {
+      return Code;
+    }
+
+  }
+}
--- CrmApp/Program.cs
+++ CrmApp/Program.cs
@@ -5,36 +5,61 @@ namespace CrmApp
   class Program
   {
     class MyClass
-      {
+    {
 
 
       public static void Main()
       {
-        string returnedProduct;
-        string message = "Give me the product name:";
+        //string returnedProduct;
+        //string message = "Give me the product name:";
+
+        Product Chocolate = new Product
+        {
+          Code = "A1",
+          Name = "Toblerone",
+          Price = 1.5m,
+          Quantity = 10
+        };
+
+         Chocolate.Print();
+
+        Product Juice = new Product
+        {
+          Code = "A2",
+          Name = "Amita",
+          Price = 1.2m,
+          Quantity = 15
+        };
+
+        Juice.Print();
 
 
-        //method call
-        returnedProduct = DoWork(message);
-        Console.WriteLine(returnedProduct);
+        decimal total;
+        total = Chocolate.GetTotalCost() + Juice.GetTotalCost();
+        Console.WriteLine("Total price of products: " + total);
+
         Console.ReadLine();
       }
 
 
 
 
-      //Declaration of a method
+
+
+
+      //Declaration of a method(s)
+
       static string DoWork(string message)
       {
 
-          string Product;
+        string Product;
 
-          Console.WriteLine("Hello Code.Hub buds!");
-          Product = Console.ReadLine();
+        Console.WriteLine("Hello Code.Hub buds!\n");
+        Product = Console.ReadLine();
 
-          //Console.WriteLine(Product);
+        //Console.WriteLine(Product);
 
-          return Product;
+        return Product;
       }
 
 
